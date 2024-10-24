db858d660d9f2da963ac073f4b2d1b8d0823b53d
JohanLMNL <j.lemesnil@gmail.com>
2024-10-24 13:00:36 +0200

    pwa config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // Désactive en développement
+  disable: process.env.NODE_ENV === 'development', // Désactiver en développement
+  register: true, // S'assurer que le service worker est bien enregistré
+  skipWaiting: true, // Forcer la mise à jour du service worker
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Si tu veux activer le mode strict de React
+  // Autres configurations Next.js ici
+};
 
 module.exports = withPWA(nextConfig);
